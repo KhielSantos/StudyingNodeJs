7eb87b0e0a20becdc381124ac5a8934d565d6b73
KhielSantos <eziquielsantos@gmail.com>
2019-03-05 13:01:59 -0300

    class 4.11

--- JS_Basics/class_4.js
+++ JS_Basics/class_4.js
@@ -21,3 +21,9 @@ console.log(hobbies);
 
 const copiedArray = [hobbies];
 console.log(copiedArray);
+
+const toArray = (arg1, arg2, arg3) => {
+  return [arg1, arg2, arg3];
+}
+
+console.log(toArray(1, 2, 3, 4));
