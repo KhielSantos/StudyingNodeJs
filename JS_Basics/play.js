c702913a950afd9e94eb1788f390b27b3ef81e3e
KhielSantos <eziquielsantos@gmail.com>
2019-03-05 12:08:08 -0300

    class 3.2

--- JS_Basics/play.js
+++ JS_Basics/play.js
@@ -4,8 +4,11 @@ const hasHobbies = true;
 
 age = 30;
 
-const summarizeUser = function(userName, userAge, userHasHobby) {
+const summarizeUser = (userName, userAge, userHasHobby) => {
   return (`Meu nome é ${userName}, tenho ${userAge} e meus hobbies são: ${userHasHobby}.`);
 }
 
+const add = (a, b) => a + b;
+
+console.log(add(1, 2));
 console.log(summarizeUser(name, age, hasHobbies));
