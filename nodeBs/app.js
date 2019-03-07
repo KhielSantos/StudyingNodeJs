23df037857a5c706a9b4f3b63d17cb59afa64f29
KhielSantos <eziquielsantos@gmail.com>
2019-03-06 23:54:53 -0300

    class 7.2

--- nodeBs/app.js
+++ nodeBs/app.js
@@ -1,13 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  // process.exit();
+  const url = req.url;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Enviar</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>Minha Pagina</title></head>');
   res.write('<body><h1>Olá seja bem vindo</h1></body>');
   res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
