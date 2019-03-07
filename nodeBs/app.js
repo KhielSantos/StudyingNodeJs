f7dd4f8c385222a5cf42c798dcf362ea326e0af9
KhielSantos <eziquielsantos@gmail.com>
2019-03-07 00:21:19 -0300

    class 7.4

--- nodeBs/app.js
+++ nodeBs/app.js
@@ -12,12 +12,21 @@ const server = http.createServer((req, res) => {
   }
 
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();
   }
-  
+
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>Minha Pagina</title></head>');
